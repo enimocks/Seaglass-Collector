798d7938f5559f687139741abe2bc73c3c52fb62
Edward Nimocks <enimocks@gmail.com>
2018-03-19 15:33:19 -0400

    more setup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,9 @@
 // VARIABLES
 // =================================================================================================
+
+// Generates a random number between 19 and 
+var randomTargetNum = Math.floor(Math.random() * 102 + 19);
+var rand1 = Math.floor(Math.random() * 12 + 1);
+var rand2 = Math.floor(Math.random() * 12 + 1);
+var rand3 = Math.floor(Math.random() * 12 + 1);
+var rand4 = Math.floor(Math.random() * 12 + 1);
