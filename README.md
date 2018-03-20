6f1af36b2985f74cff47098d2ace0b47e327bb06
Edward Nimocks <enimocks@gmail.com>
2018-03-20 13:03:28 -0400

    logic working and styling

--- README.md
+++
@@ -1,1 +0,0 @@
-# Seaglass-Collector
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,80 @@
 // VARIABLES
-// =================================================================================================
+// ======================================================================================
 
-// Generates a random number between 19 and 
+// Generates a random number between 19 and 120
 var randomTargetNum = Math.floor(Math.random() * 102 + 19);
+
+// Generates 4 random numbers, all between 1 and 12, to assign to the seaglass images
 var rand1 = Math.floor(Math.random() * 12 + 1);
 var rand2 = Math.floor(Math.random() * 12 + 1);
 var rand3 = Math.floor(Math.random() * 12 + 1);
 var rand4 = Math.floor(Math.random() * 12 + 1);
+
+var wins = 0;
+var losses = 0;
+var runningScore = 0;
+
+// Write the contents of JS variables to DOM & save assignments as variables
+var updateRand = $('#targetEl').text(randomTargetNum);
+var updateWins = $('#winsEl').text(wins);
+var updateLosses = $('#lossesEl').text(losses);
+var updateScore = $('#scoreEl').text(runningScore);
+
+// Function to determine: a Win, a Loss, Win Checker and a Reset function
+// ======================================================================================
+function reset() {
+  randomTargetNum = Math.floor(Math.random() * 102 + 19);
+  $('#targetEl').text(randomTargetNum);
+  rand1 = Math.floor(Math.random() * 12 + 1);
+  rand2 = Math.floor(Math.random() * 12 + 1);
+  rand3 = Math.floor(Math.random() * 12 + 1);
+  rand4 = Math.floor(Math.random() * 12 + 1);
+  runningScore = 0;
+  $('#scoreEl').text(runningScore);
+}
+
+function winner() {
+  wins++;
+  $('#winsEl').text(wins);
+  reset();
+}
+
+function loser() {
+  losses++;
+  $('#lossesEl').text(losses);
+  reset();
+}
+
+function checkWin() {
+  if (runningScore === randomTargetNum) {
+    winner();
+  } else if (runningScore > randomTargetNum) {
+    loser();
+  }
+}
+
+// On click events for the 4 images
+// Blue glass(1)
+$('body').on('click', '#one', function() {
+  runningScore = runningScore + rand1;
+  $('#scoreEl').text(runningScore);
+  checkWin();
+});
+// Orange glass(2)
+$('body').on('click', '#two', function() {
+  runningScore = runningScore + rand2;
+  $('#scoreEl').text(runningScore);
+  checkWin();
+});
+// Green glass(3)
+$('body').on('click', '#three', function() {
+  runningScore = runningScore + rand3;
+  $('#scoreEl').text(runningScore);
+  checkWin();
+});
+// Red glass(4)
+$('body').on('click', '#four', function() {
+  runningScore = runningScore + rand4;
+  $('#scoreEl').text(runningScore);
+  checkWin();
+});
